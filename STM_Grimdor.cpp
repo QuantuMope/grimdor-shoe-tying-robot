08d8584030d482ed685326c9da911ef79b34f9ee
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-05-04 05:03:22 -0700

    Switched clutch function into a switch format from if else statements. Adjust comment location.

--- STM_Grimdor.cpp
+++ STM_Grimdor.cpp
@@ -20,6 +20,7 @@ int j = 0;
 int p = 0;
 int t1 = 0;
 int t2 = 0;
+int clutchstep;
 int firstjump = 0;
 int firstadjust = 0;
 int firstlimitadjust = 0;
@@ -87,139 +88,151 @@ int clutch(int j)
   Direction = FORWARD;
 
   // Step numbers for each clutch case
-  if (j == 31){
-    clutchstep = 4;
-    firstjump = JUMP;
-    firstadjust = 100;
-    firstlimitadjust = 0;
-    firstINjumpback = 0;
-    firstOUTjumpback = 0;
-    p = 25;
-    t1 = 0;
-    t2 = 5000;
-    secondjump = JUMP;
-    secondlimitadjust = 1;
-    secondINjumpback = -50;
-    secondOUTjumpback = 0;
+  switch(j)
+  {
+    case 31:
+      clutchstep = 4;
+      firstjump = JUMP;
+      firstadjust = 100;
+      firstlimitadjust = 0;
+      firstINjumpback = 0;
+      firstOUTjumpback = 0;
+      p = 25;
+      t1 = 0;
+      t2 = 5000;
+      secondjump = JUMP;
+      secondlimitadjust = 1;
+      secondINjumpback = -50;
+      secondOUTjumpback = 0;
+      break;
+
+    case 13:
+      clutchstep = -4;
+      firstjump = -JUMP;
+      firstadjust = -100;
+      firstlimitadjust = 0;
+      firstINjumpback = 0;
+      firstOUTjumpback = 0;
+      p = 25;
+      t1 = 0;
+      t2 = 5000;
+      secondjump = -JUMP;
+      secondlimitadjust = -1;
+      secondINjumpback = 0;
+      secondOUTjumpback = 80;
+      break;
+
+    case 32:
+      clutchstep = 4;
+      firstjump = JUMP;
+      firstadjust = 100;
+      firstlimitadjust = 0;
+      firstINjumpback = 0;
+      firstOUTjumpback = 0;
+      p = 0;
+      t1 = 0;
+      t2 = 0;
+      secondjump = 0;
+      secondlimitadjust = 0;
+      secondINjumpback = 0;
+      secondOUTjumpback = 0;
+      break;
+
+    case 21:
+      clutchstep = 4;
+      firstjump = JUMP;
+      firstadjust = 0;
+      firstlimitadjust = 1;
+      firstINjumpback = -50;
+      firstOUTjumpback = 0;
+      p = 0;
+      t1 = 5000;
+      t2 = 0;
+      secondjump = 0;
+      secondlimitadjust = 0;
+      secondINjumpback = 0;
+      secondOUTjumpback = 0;
+      break;
+
+    case 12:
+      clutchstep = -4;
+      firstjump = -JUMP;
+      firstadjust = -100;
+      firstlimitadjust = 0;
+      firstINjumpback = 0;
+      firstOUTjumpback = 0;
+      p = 0;
+      t1 = 0;
+      t2 = 0;
+      secondjump = 0;
+      secondlimitadjust = 0;
+      secondINjumpback = 0;
+      secondOUTjumpback = 0;
+      break;
+
+    case 23:
+      clutchstep = -4;
+      firstjump = -JUMP;
+      firstadjust = 0;
+      firstlimitadjust = -1;
+      firstINjumpback = 0;
+      firstOUTjumpback = 80;
+      p = 0;
+      t1 = 5000;
+      t2 = 0;
+      secondjump = 0;
+      secondlimitadjust = 0;
+      secondINjumpback = 0;
+      secondOUTjumpback = 0;
+      break;
+
+    case 130:
+      clutchstep = -4;
+      firstjump = -JUMP;
+      firstadjust = -100;
+      firstlimitadjust = 0;
+      firstINjumpback = 0;
+      firstOUTjumpback = 0;
+      p = 25;
+      t1 = 0;
+      t2 = 5000;
+      secondjump = -JUMP;
+      secondlimitadjust = -1;
+      secondINjumpback = 0;
+      secondOUTjumpback = 80;
+      Direction = BACKWARD;
+      break;
   }
-  else if (j == 13){
-    clutchstep = -4;
-    firstjump = -JUMP;
-    firstadjust = -100;
-    firstlimitadjust = 0;
-    firstINjumpback = 0;
-    firstOUTjumpback = 0;
-    p = 25;
-    t1 = 0;
-    t2 = 5000;
-    secondjump = -JUMP;
-    secondlimitadjust = -1;
-    secondINjumpback = 0;
-    secondOUTjumpback = 80;
-  }
-  else if (j == 32){
-    clutchstep = 4;
-    firstjump = JUMP;
-    firstadjust = 100;
-    firstlimitadjust = 0;
-    firstINjumpback = 0;
-    firstOUTjumpback = 0;
-    p = 0;
-    t1 = 0;
-    t2 = 0;
-    secondjump = 0;
-    secondlimitadjust = 0;
-    secondINjumpback = 0;
-    secondOUTjumpback = 0;
-  }
-  else if (j == 21){
-    clutchstep = 4;
-    firstjump = JUMP;
-    firstadjust = 0;
-    firstlimitadjust = 1;
-    firstINjumpback = -50;
-    firstOUTjumpback = 0;
-    p = 0;
-    t1 = 5000;
-    t2 = 0;
-    secondjump = 0;
-    secondlimitadjust = 0;
-    secondINjumpback = 0;
-    secondOUTjumpback = 0;
-  }
-  else if (j == 12){
-    clutchstep = -4;
-    firstjump = -JUMP;
-    firstadjust = -100;
-    firstlimitadjust = 0;
-    firstINjumpback = 0;
-    firstOUTjumpback = 0;
-    p = 0;
-    t1 = 0;
-    t2 = 0;
-    secondjump = 0;
-    secondlimitadjust = 0;
-    secondINjumpback = 0;
-    secondOUTjumpback = 0;
-  }
-  else if (j == 23){
-    clutchstep = -4;
-    firstjump = -JUMP;
-    firstadjust = 0;
-    firstlimitadjust = -1;
-    firstINjumpback = 0;
-    firstOUTjumpback = 80;
-    p = 0;
-    t1 = 5000;
-    t2 = 0;
-    secondjump = 0;
-    secondlimitadjust = 0;
-    secondINjumpback = 0;
-    secondOUTjumpback = 0;
-  }
-  else if (j == 130){
-    clutchstep = -4;
-    firstjump = -JUMP;
-    firstadjust = -100;
-    firstlimitadjust = 0;
-    firstINjumpback = 0;
-    firstOUTjumpback = 0;
-    p = 25;
-    t1 = 0;
-    t2 = 5000;
-    secondjump = -JUMP;
-    secondlimitadjust = -1;
-    secondINjumpback = 0;
-    secondOUTjumpback = 80;
-    Direction = BACKWARD;
-  }
+
     // First initial Jump
     NEMA17->setSpeed(5);
     s28BYJ48.step(firstjump);
-    // Dual motor movement to prevent gear catching
+
+    // Dual motor "shimmy" to prevent gear catching.
     while(i < (25))
     {
-    NEMA17->step(2, FORWARD, MICROSTEP);
-    s28BYJ48.step(clutchstep);
-    i++;
+      NEMA17->step(2, FORWARD, MICROSTEP);
+      s28BYJ48.step(clutchstep);
+      i++;
     }
     i=0;
-    // Adjust for proper realignment after passing through gear
+
+    // Adjust for proper realignment after gear teeth mesh.
     s28BYJ48.step(firstadjust);
-    // Infinite clutch movement until limit switch activation
+
+    // Clutch translation until limit switch activation.
     while(i < (t1))
     {
-    s28BYJ48.step(firstlimitadjust);
-    i++;
-    if (digitalRead(limitINPin) == 0){
-      i = 5000;
-    }
-    if (digitalRead(limitOUTPin) == 0){
-      i = 5000;
-    }
+      s28BYJ48.step(firstlimitadjust);
+      i++;
+      if ((digitalRead(limitINPin) == 0) || (digitalRead(limitOUTPin) == 0))
+      {
+        i = 5000;
+      }
     }
     i=0;
-    // Jump back values after limitswitch is hit
+
+    // Jump back values after limitswitch is hit.
     s28BYJ48.step(firstINjumpback);
     s28BYJ48.step(firstOUTjumpback);
 
@@ -227,26 +240,26 @@ int clutch(int j)
     s28BYJ48.step(secondjump);
     while(i < (p))
     {
-    // Dual motor movement during clutch action to prevent gear catching
-    NEMA17->step(2, Direction, MICROSTEP);
-    s28BYJ48.step(clutchstep);
-    i++;
+      // Dual motor "shimmy" to prevent gear catching.
+      NEMA17->step(2, Direction, MICROSTEP);
+      s28BYJ48.step(clutchstep);
+      i++;
     }
     i=0;
-    // Infinite clutch movement until limit switch activation
+
+    // Clutch translation until limit switch activation.
     while(i < (t2))
     {
-    s28BYJ48.step(secondlimitadjust);
-    i++;
-    if (digitalRead(limitINPin) == 0){
-      i = 5000;
-    }
-    if (digitalRead(limitOUTPin) == 0){
-      i = 5000;
-    }
+      s28BYJ48.step(secondlimitadjust);
+      i++;
+      if ((digitalRead(limitINPin) == 0) || (digitalRead(limitOUTPin) == 0))
+      {
+        i = 5000;
+      }
     }
     i=0;
-    // Jumpback values after limitswitch is hit
+
+    // Jumpback values after limitswitch is hit.
     s28BYJ48.step(secondINjumpback);
     s28BYJ48.step(secondOUTjumpback);
 }
@@ -261,44 +274,53 @@ void loop() {
   }
   lastButton = currentButton;
 
-  // During standby, only green LED is on. When button is pressed, red LED turns on and green LED turns off.
+  // Green LED is on during standby when system is ready.
+  // When button is pressed and operation beings, red LED turns on and green LED turns off.
   digitalWrite(redledPin,ledOn);
   digitalWrite(greenledPin,!ledOn);
 
-  // Logic statement that engages if button has been pressed.
-
-  // Y-AXIS 7 INCHES ---> 5650 STEPS FORWARD IS UP!
+  // Y-AXIS 7 INCHES ---> FORWARD is up. BACKWARD is down.
   // 90° ROTATE CW   ---> 1500 STEPS FORWARD DOUBLE @ 240RPM    FROM FRONTSIDE     BACKWARDS - CLOCKWISE   FORWARDS - CCW
-  // X-AXIS          ---> use 75RPM (safe not too fast but not too slow) SINGLE is fine    - IS LEFT TO RIGHT
-  // CLUTCH          ---> + is going in - is going out
+  // X-AXIS          ---> LEFT TO RIGHT.
+  // CLUTCH          ---> + is going in - is going out.
+
   Serial.print(digitalRead(limitOUTPin));
   if (ledOn == 1)
   {
     // SHOE TYING ALGORITHM
 
+    // X-axis move out: Grab laces.
     NEMA17->setSpeed(40);
-    NEMA17->step(160,BACKWARD,DOUBLE);  // X-axis move out: Grab laces
+    NEMA17->step(160,BACKWARD,DOUBLE);
     clutch(32);
+    // Y-axis Move up: Move up past shoe.
     NEMA17->setSpeed(150);
-    NEMA17->step(6800,FORWARD,DOUBLE);  // Y-axis Move up: Move up past shoe
+    NEMA17->step(6800,FORWARD,DOUBLE);
     clutch(21);
+    // 90 degrees rotate: Have hooks facing up.
     NEMA17->setSpeed(120);
-    NEMA17->step(1400,FORWARD,DOUBLE);  // 90 degrees rotate: Have hooks facing up
+    NEMA17->step(1400,FORWARD,DOUBLE);
     clutch(13);
+    // X-axis move in: Move in until laces are past hooks.
     NEMA17->setSpeed(150);
-    NEMA17->step(260,FORWARD,DOUBLE);   // X-axis move in: Move in until laces are past hooks
+    NEMA17->step(260,FORWARD,DOUBLE);
     clutch(31);
+    // 225 degrees rotate: Rotate so that hooks can grab laces.
     NEMA17->setSpeed(150);
-    NEMA17->step(3090,FORWARD,DOUBLE);  // 225 degrees rotate: Rotate so that hooks can grab laces
+    NEMA17->step(3090,FORWARD,DOUBLE);
     clutch(13);
+    // X-axis move out: Pull out until laces are engaged in the hooks.
     NEMA17->setSpeed(40);
-    NEMA17->step(110,BACKWARD,DOUBLE);  // X-axis move out: Pull out until laces are engaged in the hooks
+    NEMA17->step(110,BACKWARD,DOUBLE);
     clutch(31);
+    // 45 Degrees rotate: Rotate down so that hooks are pointing down to avoid collision.
     NEMA17->setSpeed(120);
-    NEMA17->step(670,BACKWARD,DOUBLE);   // 45 Degrees rotate: Rotate down so that hooks are pointing down to avoid collision
-    clutch(130);                         // Clutch: 1 to 3 SPECIAL CASE
+    NEMA17->step(670,BACKWARD,DOUBLE);
+    // Clutch: 1 to 3 SPECIAL CASE
+    clutch(130);
+    // X-axis move out: move out in 8 individual spurts seperated by 500ms.
     NEMA17->setSpeed(30);
-    NEMA17->step(27,BACKWARD,DOUBLE);    // X-axis move out: move out in 8 individual spurts seperated by 500ms
+    NEMA17->step(27,BACKWARD,DOUBLE);
     delay(500);
     NEMA17->step(27,BACKWARD,DOUBLE);
     delay(500);
@@ -314,58 +336,74 @@ void loop() {
     delay(500);
     NEMA17->step(27,BACKWARD,DOUBLE);
     clutch(32);
+    // Y-axis move down: move down to be more level with shoe.
     NEMA17->setSpeed(200);
-    NEMA17->step(1000,BACKWARD,DOUBLE);  // Y-axis move down: move down to be more level with shoe
+    NEMA17->step(1000,BACKWARD,DOUBLE);
     clutch(21);
+    // 90 degrees rotate: Rotate so hooks are side to side
     NEMA17->setSpeed(120);
-    NEMA17->step(1370,FORWARD,DOUBLE);   // 90 degrees rotate: Rotate so hooks are side to side
+    NEMA17->step(1370,FORWARD,DOUBLE);
     clutch(13);
-    NEMA17->setSpeed(150);               // X-axis mvoe out: move out in 3 large spurts so that laces shoot through to the opposite side
+    // X-axis mvoe out: move out in 3 large spurts so that laces shoot through to the opposite side.
+    NEMA17->setSpeed(150);
     NEMA17->step(90,BACKWARD,DOUBLE);
     delay(500);
     NEMA17->step(90,BACKWARD,DOUBLE);
     delay(500);
     NEMA17->step(120,BACKWARD,DOUBLE);
     clutch(32);
+    // Y-axis move down:  Go down to reset.
     NEMA17->setSpeed(200);
-    NEMA17->step(6000,BACKWARD,DOUBLE);  // Y-axis move down:  Go down to reset
+    NEMA17->step(6000,BACKWARD,DOUBLE);
+    clutch(23);
 
     //*** FIRST LACE IS DONE / SECOND KNOT START ***
 
-    clutch(23);
+
+    // X-axis move in : reset.
     NEMA17->setSpeed(40);
-    NEMA17->step(215,FORWARD,DOUBLE);   // X-axis move in : reset
+    NEMA17->step(215,FORWARD,DOUBLE);
     clutch(32);
+    // Y-axis Move up: Move up past shoe.
     NEMA17->setSpeed(150);
-    NEMA17->step(6650,FORWARD,DOUBLE);  // Y-axis Move up: Move up past shoe
+    NEMA17->step(6650,FORWARD,DOUBLE);
     clutch(21);
+    // 90 degrees rotate: Have hooks facing up.
     NEMA17->setSpeed(120);
-    NEMA17->step(1175,FORWARD,DOUBLE);  // 90 degrees rotate: Have hooks facing up
+    NEMA17->step(1175,FORWARD,DOUBLE);
     clutch(13);
+    // X-axis move in:  Move in until laces are past hooks.
     NEMA17->setSpeed(150);
-    NEMA17->step(255,FORWARD,DOUBLE);   // X-axis move in:  Move in until laces are past hooks
+    NEMA17->step(255,FORWARD,DOUBLE);
     clutch(31);
+    // 225 degrees rotate: Rotate so that hooks can grab laces.
     NEMA17->setSpeed(120);
-    NEMA17->step(3000,FORWARD,DOUBLE);  // 225 degrees rotate:   Rotate so that hooks can grab laces
+    NEMA17->step(3000,FORWARD,DOUBLE);
     clutch(13);
+    // X-axis move out just in case: Move out until hooks catch laces.
     NEMA17->setSpeed(40);
-    NEMA17->step(95,BACKWARD,DOUBLE);   // X-axis move out just in case:   Move out until hooks catch laces
+    NEMA17->step(95,BACKWARD,DOUBLE);
     clutch(31);
+    // 45 Degrees rotate: rotate so that hooks are facing down to avoid collision.
     NEMA17->setSpeed(120);
-    NEMA17->step(575,BACKWARD,DOUBLE);   //45 Degrees rotate: rotate so that hooks are facing down to avoid collision
-    clutch(130);                         // Clutch: 1 to 3 SPECIAL CASE
+    NEMA17->step(575,BACKWARD,DOUBLE);
+    // Clutch: 1 to 3 SPECIAL CASE.
+    clutch(130);
+    // X-axis move out: move out in 3 spurts.
     NEMA17->setSpeed(30);
-    NEMA17->step(26,BACKWARD,DOUBLE);    // X-axis move out: move out in 3 spurts
+    NEMA17->step(26,BACKWARD,DOUBLE);
     delay(500);
     NEMA17->step(26,BACKWARD,DOUBLE);
     delay(500);
     NEMA17->step(26,BACKWARD,DOUBLE);
     clutch(32);
+    // Y-axis Move down to level with shoe.
     NEMA17->setSpeed(200);
-    NEMA17->step(1275,BACKWARD,DOUBLE);  // Y-axis Move down to level with shoe
+    NEMA17->step(1275,BACKWARD,DOUBLE);
     clutch(23);
+    // X-axis move out:  Pull until shoeknot is done.
     NEMA17->setSpeed(40);
-    NEMA17->step(290 ,BACKWARD,DOUBLE);   // X-axis move out:  Pull until Shoeknot is done
+    NEMA17->step(290 ,BACKWARD,DOUBLE);
 
     // Reverts boolean variables back to standby values.
     ledOn = 0;
